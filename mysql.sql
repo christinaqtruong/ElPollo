b9fa08944eca7c64e3e33551d5f4d45c25040bde
Christina Truong <christinaqtruong@gmail.com>
2019-07-01 00:00:19 -0500

    installed npm basics and set up database

---
+++ mysql.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS trashytrinkets_DB;
+CREATE DATABASE trashytrinkets_DB;
+
+USE trashytrinkets_DB;
+
+CREATE TABLE inventory(
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  price DEC (10,2) default 0,
+  stock_quantity INT default 0,
+  PRIMARY KEY (item_id)
+);
+INSERT INTO inventory(product_name, department_name, price, stock_quantity)
+VALUES("half-chewed gum", "consumables", 0.20, 20),
+("moldy grapefruit", "consumables", 0.99, 5), 
+("broken compass", "I want to go outside", 0.20, 3),
+("very short rope", "first aid", 5.99, 10),
+("elmers glue", "first aid", 3.99, 7),
+("hairy skull", "voodoo shit", 1.00, 8),
+("spoon with a sharpened handle", "weapons", 15.99, 30),
+("broken broom", "weapons", 10.99, 2),
+("left shoe", "I want to go outside", 8.99, 20),
+("ladder with mising rungs", "I want to go outside", 3.99, 15),
+("lightly used tampon", "hygiene", 0.69, 15),
+("two-tooth comb", "hygiene", 4.99, 10),
+("half-full Diet Pepsi", "expired beverages", 5.99, 15),
+("dented coke can", "expired beverages", 9.99, 15);
+
+SELECT * FROM inventory;
